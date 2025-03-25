bf7385062d81e79994039ac75e9feec186b57462
Admin <tuantc000@gmail.com>
2025-03-25 11:20:33 +0700

    delete past

---
+++ android/app/src/main/kotlin/com/example/flutter_lib_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_lib_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/upload_image_screen.dart
@@ -0,0 +1,97 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_lib_test/upload_image_service.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'gen/assets.gen.dart';
+
+class UploadImage extends StatefulWidget {
+  const UploadImage({super.key});
+
+  @override
+  State<UploadImage> createState() => _UploadImageState();
+}
+
+class _UploadImageState extends State<UploadImage> {
+  String isImageUploade = "";
+  bool isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 5,
+                  children: [
+                    const Text('Ảnh import và gen '),
+                    //Image.asset('${AssetGenImage('${Assets.z6024847751787148cf848f4d7d8cc6d472e9919dd7103}')}'),
+                    Expanded(
+                        child: Image.asset(Assets
+                            .z6024847751787148cf848f4d7d8cc6d472e9919dd7103
+                            .path)),
+                    SvgPicture.asset(Assets.logo),
+
+                    SvgPicture.asset(Assets.splitMode),
+                    isImageUploade == ""
+                        ? const SizedBox()
+                        : SizedBox(
+                            height: 350,
+                            width: 350,
+                            child: Image.network(
+                              isImageUploade,
+                            ),
+                          ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent),
+                      onPressed: () async {
+                        final ImagePicker picker = ImagePicker();
+                        final XFile? image =
+                            await picker.pickImage(source: ImageSource.gallery);
+                        if (image != null) {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          // it takes some time to uploade images
+                          Uint8List bytes = await image.readAsBytes();
+                          UploadApiImage()
+                              .uploadImage(bytes, image.name)
+                              .then((value) {
+                            setState(() {
+                              isImageUploade = value['location'].toString();
+                              isLoading = false;
+                            });
+                            print(
+                                "Updated Successfully with link ${value.toString()}");
+                          }).onError((error, stackTrace) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            print(error.toString());
+                          });
+                        }
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text(
+                          "Upload Image",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    );
+  }
+}
